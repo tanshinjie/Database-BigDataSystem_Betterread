1953132ee1c429cd87d8b7b5457d0337c0fff8a4
Tan Shin Jie <shinjie.work@gmail.com>
2020-10-21 01:37:20 +0800

    added cors to server

--- betterread-server/app.js
+++ betterread-server/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 5000
 
+app.use(cors())
 app.use(express.json())
+
 const review = require('./routes/review')
 
 app.use('/',review)
