9c6d6f8d1b15bd86207f6f3a743ddddc115f8fed
Tan Shin Jie <shinjie.work@gmail.com>
2020-10-21 17:57:21 +0800

    update

--- betterread-server/routes/review.js
+++ betterread-server/routes/review.js
@@ -1,33 +1,37 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.use((req,res,next)=>{
-    console.log('Logging request to MongoDB')
-    next()
-})
+router.use((req, res, next) => {
+  // TODO: Log HTTP request to MongoDB
+  console.log("Logging request to MongoDB");
+  next();
+});
 
-router.get('/',(req,res)=>{
-    // TODO: MySQL query to get all books 
-    res.send('Getting all books from MySQL')
-})
+router.get("/", (req, res) => {
+  // TODO: Get first 100 books in database from MongoDB
+  res.send("Getting first 100 books from MongoDB");
+});
 
-router.get('/book/:id',(req,res)=>{
-    // TODO: Get reviews of book with id from MySQL
-    res.send('Get list of reviews of book with ID from MySQL '+ req.params.id)
-})
+router.get("/review/:asin", (req, res) => {
+  // TODO: Get reviews of book given asin from MySQL
+  res.send(
+    "Get list of reviews for book with asin from MySQL " + req.params.asin
+  );
+});
 
-router.post('/add_book',(req,res)=>{
-    const {author, title} = req.body
-    console.log(author,title)
-    // TODO: Add book to mongoDB
-    res.send('Add book to MongoDB')
-})
+router.post("/addbook", (req, res) => {
+  const { author, title } = req.body;
+  console.log(author, title);
+  // TODO: Add book to mongoDB
+  res.send("Added book to MongoDB");
+});
 
-router.post('/add_review',(req,res)=>{
-    const {summary, review} = req.body
-    console.log(summary,review)
-    // TODO: Add review of a book to MySQL
-    res.send('Add review to MySQL database')
-})
+router.post("/addreview", (req, res) => {
+  const { summary, review, asin, ratings, reviewer } = req.body;
+  const timestamp = Date.now();
+  console.log(summary, review, asin, ratings, reviewer, timestamp);
+  // TODO: Add review of a book to MySQL
+  res.send("Added review to MySQL database");
+});
 
 module.exports = router;
