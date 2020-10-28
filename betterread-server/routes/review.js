7ec59be5167faaba2edfe832cf4b4231c97a4869
AlexisEi <56182945+AlexisEi@users.noreply.github.com>
2020-10-28 13:04:34 +0800

    Mongo Queries updated (#12)
    * Mongo Queries updated
    
    - HTTP Logs
    - Retrieve first 100 books
    - Add book into dbproj/firstcollection
    
    * Update review.js
    
    - moved logging to correct function
    - canged end point for 100 books to "/"
    
    * Mongo Updates 24/10 12.01
    
    - minor changes to res.send statements
    
    * Mongo query uodate w author,title
    
    - Line 49 does not work without "/books"
    - returns object Object when path is "/"
    - returns author and titles with "/books"
    
    * Update review.js 26Oct 1.24pm
    
    - Able to GET books with author and title with "/"
    - [object Object] returned for logs.
    -logs recorded in collection
    
    Co-authored-by: Tan Shin Jie <shinjie.work@gmail.com>

--- betterread-server/routes/review.js
+++ betterread-server/routes/review.js
@@ -1,63 +1,126 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+const MongoConnection = require("../MongoConnect");
+//Connect to MongoDB
+MongoConnection.connectToDB();
+const client = MongoConnection.client;
 
-let mysql = require('mysql');
+let mysql = require("mysql");
 let connection = mysql.createConnection({
-    host: '54.166.186.251',
-    port: 3340,
-    user: 'shinjie',
-    password: 'shinjie',
-    database: 'kindle_reviews'
+  host: "54.166.186.251",
+  port: 3340,
+  user: "shinjie",
+  password: "shinjie",
+  database: "kindle_reviews",
 });
-connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-  
-    console.log('Connected to the MySQL server.');
+connection.connect(function (err) {
+  if (err) {
+    return console.error("error: " + err.message);
+  }
+
+  console.log("Connected to the MySQL server.");
 });
 
-
-router.use((req, res, next) => {
+router.all("*", async (req, res, next) => {
   // TODO: Log HTTP request to MongoDB
+  const logs = req.body;
+  console.log(logs);
+  try {
+    const collection = client.db("dblogs").collection("logsRecord");
+    let time = Date.now;
+    console.log(time);
+    const result = {
+      timeStamp: {
+        type: "Date",
+        default: new Date(),
+      },
+      reqType: {
+        type: "String",
+        default: logs.reqType,
+      },
+      responseCode: {
+        type: "Number",
+        default: logs.responseCode,
+      },
+    };
+    await collection.insertOne(result);
+    console.log("Successfully inserted to database:", logs);
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+    return;
+  }
   console.log("Logging request to MongoDB");
-  next();
 });
 
 router.get("/", (req, res) => {
   // TODO: Get first 100 books in database from MongoDB
-  res.send("Getting first 100 books from MongoDB");
+  // Get data with author and title , limiting to 100
+
+  const dbName = client.db("dbproj");
+  try {
+    var query = { author: { $exists: true }, title: { $exists: true } };
+    dbName
+      .collection("firstcollection")
+      .find(query)
+      .limit(100)
+      .toArray(function (err, result) {
+        //res.send("Getting first 100 books from MongoDB");
+        console.log(result);
+        res.send(result);
+        res.status(200).send();
+        //res.send("Getting first 100 books from MongoDB");
+        //return result;
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(404).send();
+  }
 });
 
-
-router.get("/review/:asin", (req, res) => { 
+router.get("/review/:asin", (req, res) => {
   // TODO: Get reviews of book given asin from MySQL
-  const asinNumber = req.params.asin
+  const asinNumber = req.params.asin;
 
-  console.log("Get list of reviews for book with asin from MySQL " + asinNumber)
-  
-  connection.query('SELECT reviewerName,reviewText,summary,overall,unixReviewTime FROM reviews WHERE asin = "' + asinNumber +'" '  ,(err,results) => { //callback function 
-    if(err) {
-      return res.send(err)
+  console.log(
+    "Get list of reviews for book with asin from MySQL " + asinNumber
+  );
+
+  connection.query(
+    'SELECT reviewerName,reviewText,summary,overall,unixReviewTime FROM reviews WHERE asin = "' +
+      asinNumber +
+      '" ',
+    (err, results) => {
+      //callback function
+      if (err) {
+        return res.send(err);
+      } else {
+        return res.json({
+          reviews: results,
+        });
+      }
     }
-    
-    else {
-      
-      return res.json({
-        reviews: results
-        
-      })
-    }
-  })
+  );
 });
 
-
-
-router.post("/addbook", (req, res) => {
-  const { author, title } = req.body;
-  console.log(author, title);
+router.post("/addbook", async (req, res) => {
   // TODO: Add book to mongoDB
-  res.send("Added book to MongoDB");
+
+  try {
+    //const collection = client.db('dbproj').collection('firstcollection')
+    const { author, title } = req.body;
+    console.log(author, title);
+    const collection = client.db("dbproj").collection("firstcollection");
+    collection.insertOne({ author, title });
+    res.status(201).send({ messages: "Added book to MongoDB" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
 });
 
 router.post("/addreview", (req, res) => {
