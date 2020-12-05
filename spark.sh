e86fd5242c874ac4c37de3aacce0ec5391ec8878
AlexisEi <56182945+AlexisEi@users.noreply.github.com>
2020-12-05 14:14:45 +0800

    Spark setup (#24)
    * Spark.sh updated
    
    To run :
    ./spark.sh
    source ~/.profile
    
    * spark.sh updated
    
    to run:
    1) ./spark.sh
    2) source ~/.profile

---
+++ spark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "=== Starting setup for Spark ==="
+sudo apt install default-jdk scala git -y
+wget https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
+tar xvf spark-*
+sudo mv spark-3.0.1-bin-hadoop2.7 /opt/spark
+echo "export SPARK_HOME=/opt/spark" >> ~/.profile
+echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.profile
+echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.profile
+source ~/.profile
