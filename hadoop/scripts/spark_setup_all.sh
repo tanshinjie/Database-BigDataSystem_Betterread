37a6c3f72fb576fcd1a23debde4186e1fffbb23c
Ian Wan <hohouyj@gmail.com>
2020-12-06 21:56:13 +0800

    spark setup

--- hadoop/scripts/spark_setup_all.sh
+++ hadoop/scripts/spark_setup_all.sh
@@ -1,18 +1,63 @@
+#Run as user hadoop on namenode
 
-#! /bin/bash
-chmod +x script/spark_setup.sh
-./spark_setup.sh
+#########Download##########
+cd ~
+mkdir download
+cd download
+wget https://apachemirror.sg.wuchna.com/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
+tar zxvf spark-3.0.1-bin-hadoop3.2.tgz
 
-echo "Moving spark_setup.sh to worker nodes"
-sudo su "scp -r scripts/spark_setup.sh hadoop@d1:~/" hadoop
-sudo su "scp -r scripts/spark_setup.sh hadoop@d1:~/" hadoop
-scp -r scripts/spark_setup.sh hadoop@d2:~/
+#########Configure#########
+cp spark-3.0.1-bin-hadoop3.2/conf/spark-env.sh.template /spark-3.0.1-bin-hadoop3.2/conf/spark-env.sh
+echo "
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/opt/hadoop-3.3.0
+export SPARK_HOME=/opt/spark-3.0.1-bin-hadoop3.2
+export SPARK_CONF_DIR=\${SPARK_HOME}/conf
+export HADOOP_CONF_DIR=\${HADOOP_HOME}/etc/hadoop
+export YARN_CONF_DIR=\${HADOOP_HOME}/etc/hadoop
+export SPARK_EXECUTOR_CORES=1
+export SPARK_EXECUTOR_MEMORY=2G
+export SPARK_DRIVER_MEMORY=1G
+export PYSPARK_PYTHON=python3
+" >> spark-3.0.1-bin-hadoop3.2/conf/spark-env.sh
 
-##################################################
-echo "run spark_setup.sh on all worker nodes"
+WORKERS="d1 d2" #edit this in script generator
+for ip in ${WORKERS};
+do echo -e "${ip}" >> spark-3.0.1-bin-hadoop3.2/conf/slaves;
+done
 
-ssh hadoop@d1 "./spark_setup.sh"
+########Deploy to workers########
+tar czvf spark-3.0.1-bin-hadoop3.2.tgz spark-3.0.1-bin-hadoop3.2/
+for i in ${WORKERS};
+do scp spark-3.0.1-bin-hadoop3.2.tgz $i:./spark-3.0.1-bin-hadoop3.2.tgz;
+   #scp scripts/spark_install.sh $i:~/;
+   echo 'installing on WORKERS'
+   ssh hadoop@$i 'cd ~'
+   ssh hadoop@$i 'tar zxvf spark-3.0.1-bin-hadoop3.2.tgz'
+   ssh hadoop@$i 'sudo mv spark-3.0.1-bin-hadoop3.2 /opt/'
+   ssh hadoop@$i 'sudo chown -R hadoop:hadoop /opt/spark-3.0.1-bin-hadoop3.2'
+#######################spark_install.sh############################
+## cd ~                                                          ##
+## tar zxvf spark-3.0.1-bin-hadoop3.2.tgz                        ##
+## sudo mv spark-3.0.1-bin-hadoop3.2 /opt/                       ##
+## sudo chown -R hadoop:hadoop /opt/spark-3.0.1-bin-hadoop3.2    ##
+##################################################################
+done
+mv spark-3.0.1-bin-hadoop3.2.tgz ~/.
 
-ssh hadoop@d2 "./spark_setup.sh"
+###############Install on namenode#############
+cd ~
+tar zxvf spark-3.0.1-bin-hadoop3.2.tgz
+sudo mv spark-3.0.1-bin-hadoop3.2 /opt/
+sudo chown -R hadoop:hadoop /opt/spark-3.0.1-bin-hadoop3.2
 
-echo Done.
+############START#############
+/opt/hadoop-3.3.0/sbin/start-dfs.sh && /opt/hadoop-3.3.0/sbin/start-yarn.sh
+/opt/spark-3.0.1-bin-hadoop3.2/sbin/start-all.sh
+echo "NAMENODE JPS"
+jps
+for j in ${WORKERS};
+do echo 'WORKERNODE JPS'
+   ssh hadoop@$j 'jps'
+done
